2c6c9effc8d835ab4d0bdf35ddcd712032e332af
Uliana <uosintseva@gmail.com>
2023-04-11 21:42:56 +0300

    add and change files

---
+++ src/main/java/ru/netology/Book.java
@@ -0,0 +1,17 @@
+package ru.netology;
+public class Book extends Product {
+
+    private String name;
+    private String author;
+    private int price;
+    private int id;
+
+
+
+    public Book(int id, String name, int price, String author) {
+        super(id, name, price);
+        this.author = author;
+
+    }
+
+}
---
+++ src/main/java/ru/netology/Smartphone.java
@@ -0,0 +1,15 @@
+package ru.netology;
+
+public class Smartphone extends Product {
+    private String name;
+    private String vendor;
+    private int price;
+    private int id;
+
+    public Smartphone(int id, String name, int price, String vendor) {
+        super(id, name, price);
+        this.vendor = vendor;
+
+
+    }
+}
